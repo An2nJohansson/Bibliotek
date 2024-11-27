fa95ec680474d7255c2fa3c6f4f50c8809a61d83
An2nJohansson <an2n_johansson@outlook.com>
2024-11-27 21:47:03 +0100

    update user

--- BiblanMain/Classes/signup.cs
+++ BiblanMain/Classes/signup.cs
@@ -8,21 +8,22 @@ namespace BiblanMain.Classes
 {
     internal class SignupClass
     {
-        //Medlemsvariabler
-        public string userName;
-        public string password;
-        public string admin;
+        
+        public string username { get; set; }
+        public string password { get; set; }
+        
+        public int admin { get; set; } // användare = 0, admin= 1
 
         //Defaultkonstruktor
+        public SignupClass() { }
 
-        public SignupClass() 
+        //Konstruktor med 3 inmatade värden.
+        public SignupClass(string Username, string Password, int Admin) 
         {
-            
-        
+            username = Username;
+            password = Password;
+            admin = Admin;
         }
-
-
-
 
 
 
--- BiblanMain/Program.cs
+++ BiblanMain/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BiblanMain.Classes;
 using static System.Console;
 
 WriteLine("---------------------------------------");
 WriteLine("Välkommen till Johanssons Bibliotek! :)");
 WriteLine("---------------------------------------");
 
+List<SignupClass> usernames = new List<SignupClass>(); //Lista för användare/admin
 
 //while loop för meny
 while (true)
@@ -23,21 +25,63 @@ while (true)
         switch (number)
         {
             case 1:
+                WriteLine("Registrera användare eller administratör");
+                WriteLine("Ange önskat användarnamn:");
+                string username = ReadLine();
 
-                
-                
+                WriteLine("Ange önskat lösenord:");
+                string password = ReadLine();
+
+                WriteLine("Är du en administratör från biblioteket? ( 0 = nej, 1 = ja )");
+                //if else för att kontrollera att admin input är korrekt
+                if (int.TryParse(ReadLine(), out int admin) && (admin == 0 || admin == 1))
+                {
+                    usernames.Add(new SignupClass(username, password, admin));
+                    WriteLine("Registreringen lyckades, välkommen!:)");
+                }
+                else
+                {
+                    WriteLine("Ogiltig input för administratör, vänligen ange 0 eller 1.");
+                }
                 break;
 
             case 2:
                 Clear();
                 WriteLine("Logga in.");
-
-                bool wrong = false;//bool för att kontrollera om login är rätt.
-
+                WriteLine("Ange ditt användarnamn: ");
+                string loginUsername = ReadLine();
                 
-                if (!wrong)
+                WriteLine("Ange ditt lösenord: ");
+                string loginPassword = ReadLine();
+
+                // for loop för att kontrollera lista med användare
+                SignupClass user = null;
+
+                for (int i = 0; i < usernames.Count; i++)
                 {
-                    WriteLine("Fel användarnamn eller lösenord, försök igen.");
+                    if (usernames[i].username == loginUsername)
+                    {
+                        user = usernames[i];
+                        break; 
+                    }
+                }
+
+                // Kontrollera uppgifter, if else för att skriva ut ifall det lyckades eller ej
+                if (user != null && user.password == loginPassword)
+                {
+                    
+                    if (user.admin == 1)
+                    {
+                        WriteLine($"Inloggningen lyckades! Välkommen, administratör {user.username}!");
+                    }
+                    else
+                    {
+                        WriteLine($"Inloggningen lyckades! Välkommen {user.username}!");
+                    }
+                }
+                else
+                {
+                    WriteLine("Felaktigt användarnamn eller lösenord. Försök igen.");
                 }
                 break;
 
